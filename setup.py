b0fae136178c3d7e62475f159c7e6dfcea31ffb4
Shashidhar Hegde <hegdeshashidhar123@gmail.com>
2026-02-23 23:42:46 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='Shashidhar Hegde',
+    description='A medical chatbot application',
+    author_email='hegdeshashidhar123@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
