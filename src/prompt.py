c9761dfc3447cf31017a8ae48524a6e791f45e6c
Shashidhar Hegde <hegdeshashidhar123@gmail.com>
2026-02-28 18:39:23 +0530

    cicd added

--- src/prompt.py
+++ src/prompt.py
@@ -1,6 +1,8 @@
 system_prompt = (
-    "You are a helpful medical assistant answering questions about acne."
-    "Use the following retrieved documents to answer the question. If you don't know the answer, say you don't know."
+    "You are a helpful medical assistant answering questions."
+    "Use the following retrieved documents to answer the question."
+    "If question is not in the document but question is related to medical topics, try to answer based on general medical knowledge and first mention it is not related to the provided documents giving answer from outside source,"
+    "If outside of both case then say you don't know."
     "Don't know the answer, use three sentences maximum and keep the answer concise."
     "\n\n"
     "{context}"
